075066c41438d1a4c23e8502ba5875e7062ce806
Omkar Nilakhe <omkarnilakhe@gmail.com>
2025-01-20 21:18:54 +0530

    edit function improved, styling added

--- src/app/components/list-box/list-box.component.ts
+++ src/app/components/list-box/list-box.component.ts
@@ -1,116 +1,116 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
 import { addDoc, collection, collectionData, deleteDoc, doc, DocumentData, Firestore, getDoc, getDocs, query, setDoc, where } from '@angular/fire/firestore';
 import { AsyncPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { filter, from, map, Observable, of, startWith } from 'rxjs';
 import { ListItemComponent } from "../list-item/list-item.component";
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatAutocomplete, MatAutocompleteModule, MatAutocompleteTrigger} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-
-
+import { MatAutocomplete, MatAutocompleteModule, MatAutocompleteOrigin, MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-list-box',
   standalone: true,
-  imports: [NgFor, NgIf, AsyncPipe, FormsModule, NgStyle,NgClass, ListItemComponent,MatSelectModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule,
-    MatAutocompleteModule,MatButtonModule,MatIconModule],
+  imports: [NgFor, NgIf, AsyncPipe, FormsModule, NgStyle, NgClass, ListItemComponent, MatSelectModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule,
+    MatAutocompleteModule, MatButtonModule, MatIconModule, MatRippleModule],
   templateUrl: './list-box.component.html',
   styleUrl: './list-box.component.scss'
 })
-export class ListBoxComponent implements OnInit{
+export class ListBoxComponent implements OnInit, AfterViewInit {
+
   myControl = new FormControl('');
   fireStore = inject(Firestore);
-  itemCollection = collection(this.fireStore,'todos');
-  items$ = collectionData(this.itemCollection,{idField : 'id'});
-  filteredOptions$? : Observable<(DocumentData | (DocumentData & { id: string; }))[]>;
-
-
-  @ViewChild(MatAutocomplete) matpanel! : MatAutocomplete;
-
+  itemCollection = collection(this.fireStore, 'todos');
+  items$ = collectionData(this.itemCollection, { idField: 'id' });
+  filteredOptions$?: Observable<(DocumentData | (DocumentData & { id: string; }))[]>;
+  @ViewChild('trigger') trigger!: MatAutocompleteTrigger;
 
   ngOnInit(): void {
-
     this.filteredOptions$ = this.items$;
-    this.myControl.valueChanges.subscribe((value:any) => 
+    this.myControl.valueChanges.subscribe((value: any) =>
       this.filteredOptions$ = this._filter(String(value)?.toLowerCase() || '')
     );
-    
   }
 
-  newData(event : any){
-    event.code == 'Enter' && this.myControl.value !=='' ? this.addTodo(this.myControl.value || '') : null;
+  ngAfterViewInit(): void {
+    this.trigger.openPanel();
+  }
+
+  newData(event: any) {
+    event.code == 'Enter' && this.myControl.value !== '' ? this.addTodo(this.myControl?.value!) : null;
   }
 
   private _filter(value: string): Observable<(DocumentData | (DocumentData & { id: string; }))[]> {
     const filterValue = value
-    return this.items$.pipe(map(result=>result.filter(val=> String(val['text']).includes(filterValue))))
+    return this.items$.pipe(map(result => result.filter(val => String(val['text']).includes(filterValue))))
   }
 
-    addTodo(value:string): Observable<string>{
+  addTodo(value: string): Observable<string> {
+    const todoToCreate = { text: value, isCompleted: false, isChecked: false, isEditable: false }
+    const promise = addDoc(this.itemCollection, todoToCreate).then(response => {
+      return response.id
+    });
+    this.myControl.setValue('')
+    return from(promise);
+  }
 
-      const todoToCreate = { text : value, isCompleted: false, isChecked:false, isEditable: false}
-      const promise = addDoc(this.itemCollection, todoToCreate).then(response=>{
-        return response.id
-      });
-      this.myControl.setValue('')
-      return from(promise);
+  removeTodo($event: any, id: string): Observable<void> {
+
+    const docRef = doc(this.fireStore, "todos/", id)
+
+    const promise = deleteDoc(docRef);
+    $event.stopPropagation()
+
+    return from(promise)
+  }
+
+  updateTodo(event: any, item: any) {
+
+    if (item['text'] === '') {
+      alert('Please enter text.')
+      document.getElementById('inputBox')?.focus();
+      return;
     }
 
-    removeTodo($event: any, id : string): Observable<void> {
+    const docRef = doc(this.fireStore, "todos/", item['id'])
 
-      const docRef = doc(this.fireStore,"todos/",id)
-
-      const promise = deleteDoc(docRef);
-      $event.stopPropagation()
-      
-      return from(promise)
+    if (item['isEditable']) {
+      item['isEditable'] = false;
+      const promise1 = setDoc(docRef, { ...item, text: item['text'], isEditable: item['isEditable'] });
+      this.items$ = collectionData(this.itemCollection, { idField: 'id' });
+      event.stopPropagation()
     }
 
-    updateTodo(event: any, item : any) {
+  }
 
-      const docRef = doc(this.fireStore,"todos/",item['id'])
+  stopEvent(event: any, item: any) {
+    event.stopImmediatePropagation()
+    event.preventDefault()
+  }
 
-      if(item['isEditable']){
-        item['isEditable'] = false;
-      const promise1 = setDoc(docRef,{...item,text:item['text'], isEditable: item['isEditable']});
-      this.items$ = collectionData(this.itemCollection,{idField : 'id'});
-      event.stopPropagation()            
-      }
-      else{
+  oncheckclick(event: any, item: any) {
+    event.stopImmediatePropagation()
+    event.preventDefault()
+  }
 
-      }
-    }
+  onexactcheckclick(event: any, item: any) {
 
-    stopEvent(event:any,item:any){
-
-      //event.currentTarget.classList.contains('editbtns')  && item['']? 
-
-      event.stopImmediatePropagation()
-      event.preventDefault()
-      
-    }
-
-    oncheckclick(event:any,item:any){
-      
-      //item['isChecked'] = !item['isChecked'];
-      event.stopImmediatePropagation()
-      event.preventDefault()
-    }
-
-    onexactcheckclick(event:any,item:any){
-      
+    if (!item['isEditable'])
       item['isChecked'] = !item['isChecked'];
-      event.stopImmediatePropagation()
-    }
+    event.stopImmediatePropagation()
+  }
 
-    oneditclick( event : any, item :any){
-      item['isEditable'] = true;
-      event.stopImmediatePropagation()
-      
-    }
+  oneditclick(event: any, item: any) {
+    event.stopImmediatePropagation()
+    item['isEditable'] = true;
+    setTimeout(() => {
+      let input = document.getElementById(`inputBox${item['id']}`);
+      input?.focus();
+    }, 500);
+  }
 
 }
